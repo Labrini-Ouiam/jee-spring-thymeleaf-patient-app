de4b8ad2dd81da48f93c822d3e7c09692f4ba578
LABRINI <labriniouiam@gamil.com>
2025-03-19 21:22:07 +0100

    Ajout des entités, repositories, PatientController et page HTML du modèle

--- src/main/java/labrini/ouiam/hopital/repository/PatientRepository.java
+++ src/main/java/labrini/ouiam/hopital/repository/PatientRepository.java
@@ -1,7 +1,17 @@
 package labrini.ouiam.hopital.repository;
 
 import labrini.ouiam.hopital.entities.Patient;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Page<Patient> findByNameContaining(String name, Pageable pageable);
+
+//    @Query("select p from Patient p where p.name like :x")
+//    Page<Patient> chercher(@Param("x") String name);
+
 }
